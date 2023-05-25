e6f896c49c908afe3d81a7f8d8da8ddc20569303
Abhideep Kumar <itsabhideep@gmail.com>
2023-05-25 16:46:56 +0530

    fixed CORS and added messege for no result

--- pages/news/[slugs].js
+++ pages/news/[slugs].js
@@ -3,14 +3,16 @@ import { useRouter } from "next/router";
 
 const News = () => {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState("mt-10 text-center hidden");
   const router = useRouter();
   const { slugs } = router.query;
   const url = `https://newsapi.org/v2/everything?apiKey=70d953a5a90d4092b11bd270b65c7b69&sortBy=relevancy&language=en&q=${slugs}`;
+  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
 
   useEffect(() => {
     const getNews = async () => {
       try {
-        const response = await fetch(url);
+        const response = await fetch(proxyUrl + url);
         if (response.ok) {
           const data = await response.json();
           console.log("data", data);
@@ -19,6 +21,7 @@ const News = () => {
           throw new Error("Request failed with status " + response.status);
         }
       } catch (err) {
+        setLoading("mt-10 text-center visible")
         console.log(err);
       }
     };
@@ -29,32 +32,36 @@ const News = () => {
   }, [slugs]);
 
   return (
-    <main className="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-6 text-white">
-      {data.map((article) => (
-        <a key={article.url} href={article.url}>
-          <div className="bg-teal-600 rounded-3xl px-10 py-3">
-            <div className="">
-              <h3 className="bg-teal-950 px-5 rounded-lg">
-                Author: {article.author || "Unknown"}
-              </h3>
-              <h3 className=" px-5 rounded-lg">
-                Website: {article.source.name || "Unknown"}
-              </h3>
+    <main>
+      <h1 className={loading}>News not available for given Keyword</h1>
+      <h2 className={loading}>Come back after</h2>
+      <div className="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-6 text-white ">
+        {data.map((article) => (
+          <a key={article.url} href={article.url}>
+            <div className="bg-teal-600 rounded-3xl px-10 py-3">
+              <div className="">
+                <h3 className="bg-teal-950 px-5 rounded-lg">
+                  Author: {article.author || "Unknown"}
+                </h3>
+                <h3 className=" px-5 rounded-lg">
+                  Website: {article.source.name || "Unknown"}
+                </h3>
+              </div>
+              <h1 className="block font-bold text-xl py-3 bg-teal-900 rounded-2xl text-center p-1 ">
+                {article.title || "No Title Available"}
+              </h1>
+              <img
+                className="aspect-video py-2 rounded-2xl"
+                src={
+                  article.urlToImage ||
+                  "https://images.pexels.com/photos/5082579/pexels-photo-5082579.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                }
+                alt="Thumbnail"
+              ></img>
             </div>
-            <h1 className="block font-bold text-xl py-3 bg-teal-900 rounded-2xl text-center p-1 ">
-              {article.title || "No Title Available"}
-            </h1>
-            <img
-              className="aspect-video py-2 rounded-2xl"
-              src={
-                article.urlToImage ||
-                "https://images.pexels.com/photos/5082579/pexels-photo-5082579.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-              }
-              alt="Thumbnail"
-            ></img>
-          </div>
-        </a>
-      ))}
+          </a>
+        ))}
+      </div>
     </main>
   );
 };
